988c696fb900f533ceb567e6e2af26c29558b5b0
Santiago <santiagoreyesmarin1986@gmail.com>
2022-07-26 13:02:39 -0500

    typescrip + express

---
+++ api/models/server.ts
@@ -0,0 +1,22 @@
+import express, {Application} from "express";
+
+class Server {
+
+    private app: Application;
+    private port: string;
+
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || '8000';
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log( 'Server is listening on port' + this.port );
+        })
+    }
+
+}
+
+export default Server
